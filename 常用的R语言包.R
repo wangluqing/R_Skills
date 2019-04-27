8315fa0919cb076857c918e5af39a04b029e8d1f
王路情 <wangluqing@hzed.com>
2019-04-27 16:43:30 +0800

    常用的R包

---
+++ 常用的R语言包.R
@@ -0,0 +1,15 @@
+library(pacman) # R包管理器
+
+# 1 tidyverse包 数据科学套件
+p_load(tidyverse)
+# library(tidyverse)
+# 2 scorecard 包 评分卡包
+p_load(scorecard)
+# 3 car 包  变量集共线性问题检测
+p_load(car)
+# 4 gbdt 算法实现的R包 gbm
+p_load(gbm)
+# 5 xgboost 算法实现的R包 xgboost 
+p_load(xgboost)
+# 6 caret包 提供分类和回归任务的统一接口
+p_load(caret)
